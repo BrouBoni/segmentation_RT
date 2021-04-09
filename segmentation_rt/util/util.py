171676ddbb56c17634ae7f37041fac2f44e6b1a7
Kevin BB <kevin.brouboni@gmail.com>
2021-04-09 23:27:35 +0200

    improve codev

--- segmentation_rt/util/util.py
+++ segmentation_rt/util/util.py
@@ -31,7 +31,7 @@ def format_log(epoch, iteration, errors, t, prefix=True):
     :param t: time.
     :type t: float
     :param prefix: if prefix.
-    :type prefix: str
+    :type prefix: bool
     :return: message.
     :rtype: str.
     """
@@ -41,23 +41,6 @@ def format_log(epoch, iteration, errors, t, prefix=True):
     for k, v in errors.items():
         message += '%s: %.3f ' % (k, v)
     return message
-
-
-def parse_gpu_ids(gpu_ids):
-    """Parse gpu device.
-
-    :param gpu_ids: "-1" for cpu, "0" for the first gpu. "1,3" for the second and fourth gpu.
-    :type gpu_ids: str
-    :return: list of devices.
-    :rtype: list[int]
-    """
-    str_ids = gpu_ids.split(',')
-    gpu_ids = []
-    for str_id in str_ids:
-        gpu_id = int(str_id)
-        if gpu_id >= 0:
-            gpu_ids.append(gpu_id)
-    return gpu_ids
 
 
 def listdir_full_path(path):
@@ -115,21 +98,6 @@ def save_image(image, path, width=10, bitdepth=8, start=None, end=None, added_sl
             writer.write(f, array2list)
 
 
-def save_png(array, path):
-    """Save array as png.
-
-    :param array: Numpy array.
-    :type array:
-    :param path: name of the file.
-    :type path: str
-    """
-    with open(path, 'wb') as f:
-        writer = png.Writer(width=array.shape[1], height=array.shape[0], bitdepth=16, greyscale=True)
-        array = array.astype(np.uint16)
-        array2list = array[:, :].tolist()
-        writer.write(f, array2list)
-
-
 def get_subjects(path, structures, transform):
     """Browse the path folder to build a dataset. Folder must contains Subjects with the CT and masks.
 
@@ -160,39 +128,3 @@ def get_subjects(path, structures, transform):
         subjects.append(subject)
 
     return tio.SubjectsDataset(subjects, transform=transform)
-
-
-def parse_opt_file(opt_path):
-    """Parse option at opt_path, Return option.
-
-    :param opt_path: option path.
-    :type opt_path: str
-    """
-
-    def parse_val(s):
-        if s == 'None':
-            return None
-        if s == 'True':
-            return True
-        if s == 'False':
-            return False
-        if s == 'inf':
-            return float('inf')
-        try:
-            float_value = float(s)
-            # special case
-            if '.' in s:
-                return float_value
-            i = int(float_value)
-            return i if i == float_value else float_value
-        except ValueError:
-            return s
-
-    with open(opt_path) as f:
-        opt = dict()
-        for line in f:
-            if line.startswith('-----'):
-                continue
-            k, v = line.split(':')
-            opt[k.strip()] = parse_val(v.strip())
-    return opt
--- tests/util/test_util.py
+++ tests/util/test_util.py
@@ -2,7 +2,7 @@ import shutil
 from unittest import TestCase
 import numpy as np
 import os
-from segmentation_rt.util.util import listdir_full_path, save_image
+from segmentation_rt.util.util import listdir_full_path, save_image, print_log, format_log
 
 
 class Test(TestCase):
@@ -17,6 +17,25 @@ class Test(TestCase):
     def test_save_image(self):
         array = np.arange(90, dtype=np.int16).reshape((3, 3, 10))
         os.makedirs("saved_images", exist_ok=True)
+        save_image(array, "saved_images")
+        images = os.listdir("saved_images")
+        self.assertEqual(len(images), 10)
+
+    def test_save_image_sliced(self):
+        array = np.arange(90, dtype=np.int16).reshape((3, 3, 10))
+        os.makedirs("saved_images", exist_ok=True)
         save_image(array, "saved_images", start=3, end=7)
         images = os.listdir("saved_images")
         self.assertEqual(len(images), 4)
+
+    def test_print_log(self):
+        save_path = "results.txt"
+        out_f = open(save_path, 'w')
+        print_log(out_f, "hello world")
+        self.assertTrue(os.path.exists(save_path))
+        os.remove(save_path)
+
+    def test_format_log(self):
+        errors = {1.0: 3.14}
+        message = format_log(1, 20, errors, 22.22)
+        self.assertEqual(message, '(epoch: 1, iteration: 20, time: 22.220) 1.0: 3.140 ')
